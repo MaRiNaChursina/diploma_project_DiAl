52c014df9f4cb5f7c67d825c8ec4206acb719785
MaRiNaChursina <78023245+MaRiNaChursina@users.noreply.github.com>
2023-04-14 17:10:56 +0300

    Add files via upload

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+﻿import React from "react";
+
+function Card(props) {
+   const {card} = props;
+  return (
+    <div className="cards">
+        <img src={card.imgCard}/>
+       <h2>{card.title}</h2>
+       <p>{card.price}</p>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/data/bd.js
@@ -0,0 +1,20 @@
+﻿export default[ 
+    {
+        "id":0,
+        "imgCard":"./imges/products/soap.png",
+        "title":"Название продукта",
+        "price":"9999 Руб."
+    },
+    {
+        "id":1,
+        "imgCard":"./imges/products/soap.png",
+        "title":"Название продукта2",
+        "price":"1111 Руб."
+    },
+    {
+        "id":2,
+        "imgCard":"./imges/products/soap.png",
+        "title":"Название продукта3",
+        "price":"222 Руб."
+    }
+]
