1fd0a5d3b38969a0573a1edc9f0ccf26c95a3584
Marina_2.0 <chursina0121@gmail.com>
2023-04-18 16:51:13 +0300

    Carousel

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 ﻿import React, { useState } from "react";
 import MyButton from "./UI/button/MyButton";
 import Modal from "../components/UI/Modal/Modal";
+import Carousel from "./UI/Carousel/Carousel";
 
 function Card(props) {
    const {card} = props;
@@ -12,9 +13,20 @@ function Card(props) {
        <p>Примерная цена:  {card.price} ₽</p> 
        <button onClick={() => setModalActive(true)}>Подробнее</button>
        <Modal  active={modalActive} setActive={setModalActive}>
-          <div className="model__content-img">
-            <img src={card.imgCard}/>
-          </div>
+          <Carousel>
+            <div className="item item-1 model__content-img">
+              <img src={card.imgCard}/>
+            </div>
+            <div className="item item-2 model__content-img">
+              <img src={card.imgCard}/>
+            </div>
+            <div className="item item-3 model__content-img">
+              <img src={card.imgCard}/>
+            </div>
+            <div className="item item-4 model__content-img">
+              <img src={card.imgCard}/>
+            </div>
+          </Carousel>
           <h2 className="model__content-title">{card.title}</h2>
           <p className="model__content-description">Примерная цена:  {card.price} ₽</p>         
        </Modal>
---
+++ src/components/UI/Carousel/Carousel.js
@@ -0,0 +1,58 @@
+﻿import React, { Children, cloneElement, useEffect, useState } from "react";
+import {FaChevronLeft, FaChevronRight} from "react-icons/fa";
+import "./Carousel.css";
+
+const ITEM_WINDOW = 450;
+
+export default function Carousel ({children}) {
+
+    const[items, setItems]=useState([])
+    const[offset, setOffset] = useState(0)
+
+    const handleLeftArrowClick = () =>{
+        console.log('handleLeftArrowClick');
+        setOffset(currentOffset => {
+            const newOffset = currentOffset + ITEM_WINDOW
+            console.log( newOffset)
+            return Math.min(newOffset, 0)
+        })
+    }
+
+    const handleRightArrowClick = () =>{
+        console.log('handleRightArrowClick');
+        setOffset((currentOffset)=>{
+           const newOffset = currentOffset - ITEM_WINDOW
+           const maxOffset = -(ITEM_WINDOW * (items.length - 1))
+           console.log(newOffset, maxOffset)
+           return Math.max(newOffset, maxOffset)
+        })
+    }
+
+    useEffect(()=>{
+        setItems(
+            Children.map(children, child => {
+                return cloneElement(child,{
+                    style:{
+                        height:'100%',
+                        minWidth:`${ITEM_WINDOW}px`,
+                        maxWidth:`${ITEM_WINDOW}px`,
+                    },
+                })
+            })
+        )
+    },[])
+
+
+
+   return( 
+         <div className="main-container">
+            <FaChevronLeft className="arrow" onClick={handleLeftArrowClick}/>
+                <div className="window">
+                    <div className="all-items-container" style ={{transform:`translateX(${offset}px)`}}>
+                        {items}
+                    </div>
+                </div> 
+            <FaChevronRight className="arrow" onClick={handleRightArrowClick}/>
+        </div>
+    )
+}
